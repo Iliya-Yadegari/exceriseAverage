db214abf5d2773f810cd402d55ca6664c922e7fb
Iliya-Yadegari <naruto3@email.com>
2021-06-21 19:59:17 +0100

    Create averageCode.py

---
+++ averageCode.py
@@ -0,0 +1,11 @@
+exercise1 = int(input('Name of exercise 1: '))
+score_exercise1 = int(input('Score received for exercise 1: '))
+total_exercise1 = int(input('Total points possible for exercise 1: '))
+
+exercise2 = int(input('Name of exercise 2: '))
+score_exercise2 = int(input('Score received for exercise 2: '))
+total_exercise2 = int(input('Total points possible for exercise 2: '))
+
+exercise3 = int(input('Name of exercise 3: '))
+score_exercise3 = int(input('Score received for exercise 3: '))
+total_exercise3 = int(input('Total points possible for exercise 3: '))
